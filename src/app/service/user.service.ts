1c91710050c8e7333a3bed6d50cfe9057372e063
vinay951 <51898725+vinay951@users.noreply.github.com>
2024-12-29 23:28:17 +0530

    Update user.service.ts

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -8,7 +8,7 @@ import { Login, User } from '../model';
 })
 export class UserService {
 
-  private apiUrl = 'http://localhost:8080';  // Replace with your actual API
+  private apiUrl = 'https://backend-1055536593121.us-central1.run.app';  // Replace with your actual API
 
   constructor(private http: HttpClient) { }
 
