2045a3a091271570ee39c4ae33ebcdf6f07f67a6
Vinay kumar Reddy <vinaykumarreddy@Vinays-MacBook-Air.local>
2025-01-21 11:29:40 +0530

    changes

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -8,16 +8,14 @@ export const authGuard: CanActivateFn = (route, state) => {
   sessionService.setSessionData("routed",state.url);
   const exp = sessionService.getSessionData("expire");
   var timeExpired = false;
-  if (!exp) return true; // If no token is found, consider it expired
   try {
     const expirationTime = Number(sessionService.getSessionData("expire")) * 1000; // exp is in seconds, convert to milliseconds
-    timeExpired =  Date.now() > expirationTime; // If current time is greater than expiration time, it's expired
+    timeExpired =  !(Date.now() > expirationTime); // If current time is greater than expiration time, it's expired
   } catch (error) {
-    timeExpired =  false; // If token decoding fails, consider it expired
+    timeExpired =  true; // If token decoding fails, consider it expired
   }
-
   // Check if authToken exists in localStorage
-  if (localStorage.getItem('user') && !timeExpired) {
+  if (localStorage.getItem('user') && timeExpired) {
     if(state.url === '/test' || state.url === '/profile'){
       if(localStorage.getItem('user')?.startsWith("User-")){
         router.navigate(['/accessDenied']);
