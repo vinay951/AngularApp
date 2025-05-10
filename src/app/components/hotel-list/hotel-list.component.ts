48870267d01fa79480022458d1aef234e86b5afe
Vinay kumar Reddy <vinaykumarreddy@Vinays-MacBook-Air.local>
2025-05-10 12:38:59 +0530

    changes

--- src/app/components/hotel-list/hotel-list.component.ts
+++ src/app/components/hotel-list/hotel-list.component.ts
@@ -2,18 +2,20 @@ import { Component, Input, OnInit } from '@angular/core';
 import { GooglePlacesService } from '../../services/google-places.service';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { LoadingComponent } from "../../loading/loading.component";
 
 @Component({
   selector: 'app-hotel-list',
   templateUrl: './hotel-list.component.html',
   styleUrls: ['./hotel-list.component.css'],
-  imports: [FormsModule, CommonModule, ReactiveFormsModule]
+  imports: [FormsModule, CommonModule, ReactiveFormsModule, LoadingComponent]
 })
 export class HotelListComponent implements OnInit {
   hotels: any[] = [];
   lat!: number ;  // Example: San Francisco
   lng!: number;
   Math: any;
+isDataLoading = false;
 
   constructor(private googlePlacesService: GooglePlacesService) {}
 
@@ -23,8 +25,10 @@ export class HotelListComponent implements OnInit {
     this.getHotels();
   }
   getHotels(): void {
+    this.isDataLoading = true;
     this.googlePlacesService.getNearbyHotels(this.lat, this.lng).subscribe(response => {
       this.hotels = response.results;
+      this.isDataLoading = false;
     });
   }
   createStars(rating: number) {
